0799a4f605f51f8cfe1cd3d14f5eccd942cb6164
Peter Lai <PLai@nephilaadvisors.co.uk>
2017-02-07 15:12:09 +0000

    Inital checkin

---
+++ src/main/scala/websiteMapGenerator/siteMapGenerator.scala
@@ -0,0 +1,43 @@
+package websiteMapGenerator
+
+import scala.util.Try
+import scala.collection.mutable.Set
+
+object siteMapGenerator {
+  var listofVistedUrl = Set[String]() //List of sites visited already, so the crawler doesn't get into a infinite loop
+
+  def main(args : Array[String]) : Unit = {
+    if (args.length == 0)
+      println ("No arguments \n try siteMapGenerator http://www.programcreek.com/")
+    else if(!args(0).isEmpty){
+      println("Generating Site map for " + args(0))
+      findLink(downloadHtml(args(0)), domain = args(0).split("//")(1).split("/")(0))
+    }
+  }
+
+  def findLink(html : String, domain : String) :Unit = {
+    """href="([a-zA-Z0-9:/\.]*)?"""".r.findAllIn(html.toString).foreach({ urls =>
+      val fAndThenG = replaceHrefinString _ andThen endsWithSlash _ 
+      val cleandUpUrl: String = fAndThenG(urls)
+
+      if (!listofVistedUrl.contains(cleandUpUrl) && cleandUpUrl.contains(domain)) {
+        println("Found unique URL " + cleandUpUrl)
+        listofVistedUrl.add(cleandUpUrl)
+        findLink(downloadHtml(cleandUpUrl), domain)
+      }
+    })
+  }
+
+  def replaceHrefinString(text: String): String ={
+    "href=".r.replaceFirstIn(text, "").replace("\"", "")
+  }
+
+  def endsWithSlash(url : String): String ={
+    if (url.endsWith("/")) url.dropRight(1)
+    else url
+  }
+
+  def downloadHtml(url : String): String = {
+    Try(scala.io.Source.fromURL(url).mkString) getOrElse({System.out.println("Cannot reach page " + url); ""})
+  }
+}
